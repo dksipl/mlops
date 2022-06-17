293286ce03ed7c761de6dd998fa7972725a82f00
HmSamyaK <harsh.mankodiya.samyal@gmail.com>
2022-06-17 12:40:31 +0530

    Revert "template added"
    This reverts commit eb35824a9ef30ce8b5b36af48d9c5a49b12969d2.

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = flask.Flask(__name__)
 @app.route("/")
 def hello():
     """ Hello Jethalal Function"""
-    # return "<h1>Hello Jethalal</h1>"
-    return flask.render_template('index.html')
+    return "<h1>Hello Jethalal</h1>"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
